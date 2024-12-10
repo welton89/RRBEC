91dde27384eedec4e03369c2ddbfcfe2219a48a4
Welton Moura <weltonmoura@live.com>
2024-12-10 11:09:29 -0300

    passando dados para o html

--- gestaoRaul/mesas/views.py
+++ gestaoRaul/mesas/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def mesas(request):
-    return render(request, 'mesas.html')
+    return render(request, 'mesas.html', {'mesas': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']})
