335c5e1911db34dbd9dc971948d7db9120514509
Welton Moura <weltonmoura@live.com>
2024-12-19 11:30:08 -0300

    função imprimir fichas

--- gestaoRaul/templates/static/comandas/js/viewcomanda.js
+++ gestaoRaul/templates/static/comandas/js/viewcomanda.js
@@ -16,7 +16,21 @@ function removeCloseModal() {
     document.getElementById('remove-product-comanda').style.display = 'none';
 }
 
-
+function imprimirFichas() {
+    const element = document.getElementById("list-products-comanda");
+    if (element) {
+      var content = element.innerHTML;
+      console.log(content);
+      content = content.replace(/<button[^>]*>(?:(?!<\/button>)[\s\S])*<\/button>/gi,'');
+      const printWindow = window.open('', '_blank');
+      printWindow.document.write('<table>'+content+'</table>');
+      printWindow.document.close();
+      printWindow.print();
+      printWindow.close();
+    } else {
+      console.error(`Element with ID not found`);
+    }
+  }
 
 
 
@@ -24,7 +38,7 @@ document.getElementById('openModal').addEventListener('click', openModal);
 
 document.getElementById('productForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
-
-
 });
+
+
  
