8c41336029a4767625a33c43de2d79c65b88a94d
Welton Moura <weltonmoura@live.com>
2024-12-21 13:58:35 -0300

    feat: create home page

--- gestaoRaul/home/views.py
+++ gestaoRaul/home/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from django.db.models import Count, F
 
-# Create your views here.
+from comandas.models import ProductComanda
+from products.models import Product
+from payments.models import Payments
+
 def home(request):
-    return render(request, 'home.html')
+    total_pagamentos = Payments.objects.aggregate(total=Sum('value'))['total']
+    qdt_pagamentos = Payments.objects.aggregate(total=Count('value'))['total']
+    pagamentos = Payments.objects.all()
+
+    produtos_mais_vendidos = ProductComanda.objects.values('product').annotate(
+    quantidade=Count('product'),
+    nome=F('product__name') ).order_by('-quantidade')[:5]
+
+    return render(request, 'home.html', {'total_pagamentos': total_pagamentos, 'pagamentos': pagamentos, 'qdt_pagamentos': qdt_pagamentos, 'produtos_mais_vendidos': produtos_mais_vendidos})
