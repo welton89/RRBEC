aff5fe24d84f74ac9c4afd2ffe0f9d9e9aa0af70
Welton Moura <weltonmoura@live.com>
2025-01-14 19:31:16 -0300

    url admin na navbar

--- gestaoRaul/gestaoRaul/urls.py
+++ gestaoRaul/gestaoRaul/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
+    path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('login/', include('login.urls')),
     path('products/', include('products.urls')),
