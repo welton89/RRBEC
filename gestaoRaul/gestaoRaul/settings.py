92ebad35fee437b7ddd44ad5b82d39eae334e84b
Welton Moura <weltonmoura@live.com>
2025-07-07 14:26:29 -0300

    s

--- gestaoRaul/gestaoRaul/settings.py
+++ gestaoRaul/gestaoRaul/settings.py
@@ -106,12 +106,7 @@ WSGI_APPLICATION = 'gestaoRaul.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 
 
 DATABASES = {
