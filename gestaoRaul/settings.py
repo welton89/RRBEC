aec59990a778f639e91e83a791dca88521aefad5
Welton Moura <weltonmoura@live.com>
2026-04-05 12:07:06 -0300

    fix: update STATIC_URL to use an absolute path for correct static file resolution

--- gestaoRaul/settings.py
+++ gestaoRaul/settings.py
@@ -164,7 +164,7 @@ DATE_FORMAT = "d/m/Y - H:M"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
